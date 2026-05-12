1ca994e9804f071ff0e40ee7f5b3418ceeef9dce
Gael Pérez <ThecrafterLikecraft@gmail.com>
2026-05-12 11:23:32 -0600

    Archivo Inicial - Saludo

--- proyecto_Montecarlo/src/main/java/Proyecto_Montecarlo.java
+++ proyecto_Montecarlo/src/main/java/Proyecto_Montecarlo.java
@@ -7,5 +7,6 @@ public class Proyecto_Montecarlo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hay que chambear");
     }
 }
