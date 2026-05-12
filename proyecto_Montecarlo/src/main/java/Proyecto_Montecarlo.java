d37cc97d38747400aff65084ff8972e6c4dd6152
Manuel Gordillo <manuelgordillo2006@gmail.com>
2026-05-12 11:39:22 -0600

    gael escribio eso

--- proyecto_Montecarlo/src/main/java/Proyecto_Montecarlo.java
+++ proyecto_Montecarlo/src/main/java/Proyecto_Montecarlo.java
@@ -8,5 +8,6 @@ public class Proyecto_Montecarlo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hay que chambear");
+        System.out.println("Gael dice que laura le debe dinero ");
     }
 }
