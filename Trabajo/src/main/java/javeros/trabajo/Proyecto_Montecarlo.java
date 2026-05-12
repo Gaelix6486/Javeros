da70ed06f235391dbdaa6af9f7494057abe9a76c
Gael Pérez <ThecrafterLikecraft@gmail.com>
2026-05-12 16:47:35 -0600

    ..

--- Trabajo/src/main/java/javeros/trabajo/Proyecto_Montecarlo.java
+++ Trabajo/src/main/java/javeros/trabajo/Proyecto_Montecarlo.java
@@ -1,12 +1,10 @@
-
-package javeros.trabajo;
-
-/**
- *
- * @author thecr
- */
+import java.util.Scanner;
 public class Proyecto_Montecarlo {
 
     public static void main(String[] args) {
-    }
-}
+    System.out.println("BIENVENIDOS");
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Ingrese el numero de iteraciones: ");
+}}
