673f07a51d46fcf859c4ed35cc4bc1b3c3026637
Gael Pérez <ThecrafterLikecraft@gmail.com>
2026-05-12 14:15:52 -0600

    PRIMERA PRUEBA DE LÓGICA

--- proyecto_Montecarlo/src/main/java/Proyecto_Montecarlo.java
+++ proyecto_Montecarlo/src/main/java/Proyecto_Montecarlo.java
@@ -1,12 +1,30 @@
-
-/**
- *
- * @author thecr
- */
+import java.util.Scanner;
 public class Proyecto_Montecarlo {
 
     public static void main(String[] args) {
     System.out.println("BIENVENIDOS");
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Ingrese el numero de iteraciones");
+int iteraciones = sc.nextInt();
+double x, y;
+int dentro = 0;
+
+for (int i = 0; i < iteraciones; i++) {
+    
+    x = Math.random();
+    y = Math.random();
+
+    if (x * x + y * y <= 1) {
+        dentro++;
+    }
+}
+double pi = (double) dentro / iteraciones * 4;
+
+
+
+
  
     }
 }
